fe85bb971777a7c6bf6c8a7a297ae9b00376e545
Elisa Zuccolo <56590862+elisa82@users.noreply.github.com>
2024-11-26 08:31:20 +0100

    recovered hisada from gitignore

---
+++ create_input_hisada.py
@@ -0,0 +1,19 @@
+def create_input_hisada_run(folder, layers, fault, computational_param, sites, path_code, path_data):
+    from rapids.create_input_hisada_files import create_input_grflt12s
+    from rapids.create_input_hisada_files import create_script_hisada
+    from rapids.create_input_hisada_files import create_answers_grflt12s
+    from rapids.create_input_hisada_files import create_answers_grfftspmm
+    from rapids.create_input_hisada_files import create_answers_phs3sQ
+    import os
+
+    folder = folder + '/HISADA'
+
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+    create_input_grflt12s(folder, computational_param, layers, fault, sites)
+    create_script_hisada(folder, path_code)
+    create_answers_grflt12s(folder)
+    create_answers_grfftspmm(folder)
+    create_answers_phs3sQ(folder)
+    return
